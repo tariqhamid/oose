d61cbc80406abf5c8287220ea8ae5ac2f893236d
Tony Butler <spudz76@gmail.com>
2014-03-14 21:42:21 -0600

    WTF missed some junk?

--- serve/app.js
+++ serve/app.js
@@ -1,20 +1,48 @@
 'use strict';
-var bencode = require('bencode')
-  , os = require('os')
-  , ds = require('diskspace')
-  , dgram = require('dgram')
+var config = require('../config')
   , program = require('commander')
+  , os = require('os')
+  , fs = require('fs')
+  , mkdirp = require('mkdirp')
+  , ds = require('diskspace')
+  , bencode = require('bencode')
+  , dgram = require('dgram')
 
+//set defaults
+var DFL = {
+  dataRoot: config.get('serve.dataRoot') || './data/serve',
+  hostname: config.get('serve.hostname') || os.hostname(),
+  port: config.get('serve.port') || 3000,
+  mcastAddress: config.get('serve.mcastAddress') || '226.0.0.1',
+  mcastTTL: config.get('serve.mcastTTL') || 2,
+  mcastInterval: config.get('serve.mcastInterval') || 1000
+}
+
+//read command line overrides
 program
   .version('0.0.1')
-  .option('-h, --hostname <hostname>','Set process hostname, defaults to os hostname')
-  .option('-m, --mcast <multicast ip>','Set multicast IP, default 226.0.0.1')
+  .option('-r, --dataRoot <storage path>','Set storage path, default [' + DFL.dataRoot + ']')
+  .option('-h, --hostname <hostname>','Set process hostname, default [' + DFL.hostname + ']')
+  .option('-p, --port <listen port>','Set listen port, default [' + DFL.port + ']')
+  .option('-m, --mcastAddress <multicast IP>','Set multicast IP, default [' + DFL.mcastAddress + ']')
+  .option('-t, --mcastTTL <TTL>','Set multicast TTL (1-255), default [' + DFL.mcastTTL + ']')
+  .option('-i, --mcastInterval <msec>','Set multicast announcement interval in milliseconds, default [' + DFL.mcastInterval + ']')
   .option('-v, --verbose','Turn on logging of registrations')
   .parse(process.argv)
 
-var hostname = program.hostname || os.hostname()
-var mulicastAddress = program.mcast || '226.0.0.1'
+var dataRoot = program.dataRoot || DFL.dataRoot
+  , hostname = program.hostname || DFL.hostname
+  , port = program.port || DFL.port
+  , mcastAddress = program.mcastAddress || DFL.mcastAddress
+  , mcastInterval = program.mcastInterval || DFL.mcastInterval
+  , mcastTTL = program.mcastTTL || DFL.mcastTTL
 
+//make sure the root folder exists
+if(!fs.existsSync(dataRoot)){
+  mkdirp.sync(dataRoot)
+}
+
+//setup server side (listener)
 var server = dgram.createSocket('udp4')
 server.on('message',function(buf,rinfo){
   var heartbeat = bencode.decode(buf)
@@ -31,12 +59,19 @@ server.on('message',function(buf,rinfo){
       'free:' + heartbeat.free / 1024 +
       ']'
     )
-    console.log(rinfo)
   }
 })
-server.bind(3001,function(){
-  server.addMembership(mulicastAddress)
-  server.setMulticastTTL(255)
+server.bind(port,function(){
+  server.addMembership(mcastAddress)
+  server.setMulticastTTL(mcastTTL)
+})
+
+
+//setup client side (announcer)
+var client = dgram.createSocket('udp4')
+client.bind(function(){
+  client.addMembership(mcastAddress)
+  client.setMulticastTTL(mcastTTL)
 })
 
 var sendHeartbeat = function(){
@@ -44,19 +79,13 @@ var sendHeartbeat = function(){
     hostname: hostname,
     load: os.loadavg().toString()
   }
-  ds.check('C',function(total,free,status){
-    console.log(total,free,status)
+  ds.check(dataRoot,function(total,free,status){
     message.free = free || 0
     message.sent = new Date().getTime()
     console.log(message)
     var buf = bencode.encode(message)
-    var client = dgram.createSocket('udp4')
-    client.bind(function(){
-      client.addMembership(mulicastAddress)
-      client.setMulticastTTL(255)
-      client.send(buf,0,buf.length,3001,mulicastAddress)
-    })
-    setTimeout(sendHeartbeat,1000)
+    client.send(buf,0,buf.length,port,mcastAddress)
+    setTimeout(sendHeartbeat,mcastInterval)
   })
 }
 
