e5b27ec428d9c6734a9045428e1cb6000529c746
Tony Butler <spudz76@gmail.com>
2014-03-17 00:13:16 -0600

    Restructure a bit, more clear and simple.  Move server/client guts into the bind callback.

--- mesh/index.js
+++ mesh/index.js
@@ -8,37 +8,7 @@ var config = require('../config')
   , dgram = require('dgram')
   , path = require('path')
 
-//setup server side (listener)
-var server = dgram.createSocket('udp4')
-server.on('message',function(buf){
-  var announce = bencode.decode(buf)
-  //ignore ourselves
-  if(announce.hostname.toString() === config.get('hostname')) return
-  if(program.verbose){
-    logger.info(
-      announce.hostname +
-        ' posted a announce' +
-        ' at ' + announce.sent +
-        ':[' +
-        'load:' + announce.load +
-        '/' +
-        'free:' + announce.free / 1024 +
-        ']'
-    )
-  }
-})
-server.bind(config.get('serve.port'),function(){
-  server.addMembership(config.get('mesh.address'))
-  server.setMulticastTTL(config.get('mesh.ttl'))
-})
-
-//setup client side (announcer)
-var client = dgram.createSocket('udp4')
-client.bind(function(){
-  client.addMembership(config.get('mesh.address'))
-  client.setMulticastTTL(config.get('mesh.ttl'))
-})
-
+//utility functions
 var cpuAverage = function(){
   var totalIdle = 0
     , totalTick = 0
@@ -51,26 +21,55 @@ var cpuAverage = function(){
 }
 var lastMeasure = cpuAverage()
 var getLoad = function(){
-    var thisMeasure = cpuAverage()
-    var percentageCPU = 100 - ~~(100 * (thisMeasure.idle - lastMeasure.idle) / (thisMeasure.total - lastMeasure.total))
-    lastMeasure = thisMeasure
-    return percentageCPU
+  var thisMeasure = cpuAverage()
+  var percentageCPU = 100 - ~~(100 * (thisMeasure.idle - lastMeasure.idle) / (thisMeasure.total - lastMeasure.total))
+  lastMeasure = thisMeasure
+  return percentageCPU
 }
 
-var sendAnnounce = function(){
-  var message = {}
-  message.hostname = config.get('hostname')
-  message.load = getLoad()
-  var spacepath = path.resolve(config.get('serve.dataRoot'))
-  if('win32' === os.platform()) spacepath = spacepath.substr(0,1)
-  ds.check(spacepath,function(total,free){
-    message.free = parseInt(free,10) || 0
-    message.sent = new Date().getTime()
-    console.log(message)
-    var buf = bencode.encode(message)
-    client.send(buf,0,buf.length,config.get('serve.port'),config.get('mesh.address'))
-    setTimeout(sendAnnounce,config.get('mesh.interval'))
+//setup server side (listener)
+var server = dgram.createSocket('udp4')
+server.bind(config.get('serve.port'),function(){
+  server.addMembership(config.get('mesh.address'))
+  server.setMulticastTTL(config.get('mesh.ttl'))
+  server.on('message',function(buf){
+    var announce = bencode.decode(buf)
+    //ignore ourselves
+    if(announce.hostname.toString() === config.get('hostname')) return
+    if(program.verbose){
+      logger.info(
+        announce.hostname +
+          ' posted a announce' +
+          ' at ' + announce.sent +
+          ':[' +
+          'load:' + announce.load +
+          '/' +
+          'free:' + announce.free / 1024 +
+          ']'
+      )
+    }
   })
-}
+})
 
-sendAnnounce()
+//setup client side (announcer)
+var client = dgram.createSocket('udp4')
+client.bind(function(){
+  client.addMembership(config.get('mesh.address'))
+  client.setMulticastTTL(config.get('mesh.ttl'))
+  var sendAnnounce = function(){
+    var message = {}
+    message.hostname = config.get('hostname')
+    message.load = getLoad()
+    var spacepath = path.resolve(config.get('serve.dataRoot'))
+    if('win32' === os.platform()) spacepath = spacepath.substr(0,1)
+    ds.check(spacepath,function(total,free){
+      message.free = parseInt(free,10) || 0
+      message.sent = new Date().getTime()
+      console.log(message)
+      var buf = bencode.encode(message)
+      client.send(buf,0,buf.length,config.get('serve.port'),config.get('mesh.address'))
+      setTimeout(sendAnnounce,config.get('mesh.interval'))
+    })
+  }
+  sendAnnounce()
+})
