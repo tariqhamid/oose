edc71c8f862203336f01d1c8fd94e10fde5e73e8
Tony Butler <spudz76@gmail.com>
2014-03-16 22:46:23 -0600

    Repair CPU usage measurement, should work cross-platform.  Integers are all integers (some were string).

--- mesh/index.js
+++ mesh/index.js
@@ -39,42 +39,41 @@ client.bind(function(){
   client.setMulticastTTL(config.get('mesh.ttl'))
 })
 
-var getLoad = function(){
-  if('win32' === os.platform()){
-    require('windows-cpu').totalLoad(function(err, res){
-      var sum = 0
-      res.forEach(function(x){sum += x})
-      return sum / res.length
-    })
-  } else {
-    var load = {
-      user: 0,
-      nice: 0,
-      sys: 0,
-      idle: 0,
-      irq: 0
-    }
-    os.cpus().forEach(function(x){
-      for(var y in x.times) load[y] += x.times[y]
-    })
-    return (load.user + load.nice + load.sys) / load.idle * 100
+var cpuAverage = function(){
+  var totalIdle = 0
+    , totalTick = 0
+  var cpus = os.cpus()
+  for(var i=0,len=cpus.length; i<len; i++){
+    for(var type in cpus[i].times) totalTick += cpus[i].times[type]
+    totalIdle += cpus[i].times.idle
   }
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length}
+}
+var lastMeasure = cpuAverage()
+var getLoad = function(cb){
+  setTimeout(function(){
+    var thisMeasure = cpuAverage()
+    var percentageCPU = 100 - ~~(100 * (thisMeasure.idle - lastMeasure.idle) / (thisMeasure.total - lastMeasure.total))
+    lastMeasure = thisMeasure
+    cb(percentageCPU)
+  },100)
 }
 
 var sendAnnounce = function(){
-  var message = {
-    hostname: config.get('hostname'),
-    load: os.loadavg().toString() //getLoad()
-  }
-  var spacepath = path.resolve(config.get('serve.dataRoot'))
-  if('win32' === os.platform()) spacepath = spacepath.substr(0,1)
-  ds.check(spacepath,function(total,free){
-    message.free = free || 0
-    message.sent = new Date().getTime()
-    console.log(message)
-    var buf = bencode.encode(message)
-    client.send(buf,0,buf.length,config.get('serve.port'),config.get('mesh.address'))
-    setTimeout(sendAnnounce,config.get('mesh.interval'))
+  var message = {}
+  getLoad(function(load){
+    message.hostname = config.get('hostname')
+    message.load = load
+    var spacepath = path.resolve(config.get('serve.dataRoot'))
+    if('win32' === os.platform()) spacepath = spacepath.substr(0,1)
+    ds.check(spacepath,function(total,free){
+      message.free = parseInt(free,10) || 0
+      message.sent = new Date().getTime()
+      console.log(message)
+      var buf = bencode.encode(message)
+      client.send(buf,0,buf.length,config.get('serve.port'),config.get('mesh.address'))
+      setTimeout(sendAnnounce,config.get('mesh.interval'))
+    })
   })
 }
 
