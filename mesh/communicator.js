ef03d9b4a6a2127d192ac0e30454cab4bbb8b76b
Bryan Tong <contact@nullivex.com>
2014-03-20 16:51:43 -0600

    upgraded communicator to be an event emitter, implemented middleware stacks

--- mesh/communicator.js
+++ mesh/communicator.js
@@ -3,78 +3,154 @@
 var ObjectManage = require('object-manage')
   , bencode = require('bencode')
   , crc32 = require('buffer-crc32')
+  , EventEmitter = require('events').EventEmitter
+  , util = require('util')
+  , dgram = require('dgram')
+  , async = require('async')
 
-function Communicator(options,callbacks){
-  this.options = new ObjectManage({
-    logger: null,
-    proto: 'udp4',
-    mcast: {address: null, ttl: 1},
-    address: '127.0.0.1',
-    port: 3333
-  })
-  this.options.load(options)
-  this.socket = null
-  this.callbacks = callbacks || {
-    bind: [],
-    recv: [],
-    xmit: []
-  }
-  this.callbackReturnValues = {
-    bind: [],
-    recv: [],
-    xmit: []
+/**
+ * Encode an object to be sent
+ * @param obj
+ */
+var encode = function(obj){
+  var pkt = bencode.encode(obj)
+  return Buffer.concat([crc32(pkt),pkt])
+}
+
+/**
+ * Decode a buffer and explode into an object
+ * @param buf
+ */
+var decode = function(buf){
+  var pkt = bencode.decode(buf)
+  for(var k in pkt)
+    if(pkt.hasOwnProperty(k) && Buffer.isBuffer(pkt[k]))
+      pkt[k] = pkt[k].toString()
+  return pkt
+}
+
+/**
+ * Communicator constructor, accepts options
+ * @param options
+ * @constructor
+ */
+var Communicator = function(options){
+  var self = this
+  //init event emitter
+  EventEmitter.call(self)
+  //setup options
+  self.options = new ObjectManage(self.optionSchema)
+  self.options.load(options)
+  self.socket = null
+
+  var setup = {
+    udp4: function(){
+      self.socket = dgram.createSocket('udp4')
+    },
+    mcast: setup.udp4,
+    tcp4: function(){
+      self.emit('error','TCP not yet supported')
+    }
   }
   //define the recv and xmit sockets
-  switch(this.options.proto){
-  case 'udp4':
-  case 'mcast':
-    var dgram = require('dgram')
-    this.socket = dgram.createSocket('udp4')
-    if('mcast' === this.options.proto)
-      //push the bind callback for multicast setup
-      this.callbacks.bind.shift(function(){
-        this.socket.addMembership(this.options.mcast.address)
-        this.socket.setMulticastTTL(this.options.mcast.ttl)
-        return true
-      }.bind(this))
-    break
-  case 'tcp4':
-    //TODO some tcp majiks
-    break
-  }
+  setup[self.options.get('proto')]()
 
-  this.socket.bind(this.options.port,function(){
-    this.callbacks.bind.some(function(cb){
-      return this.callbackReturnValues.bind.push(cb())
-    }.bind(this))
-    this.socket.on('message',function(buf,rinfo){
+  //bind the socket, setup to parse messages and fire receive event on incoming
+  self.socket.bind(self.options.get('port'),function(){
+    //add multicast membership if needed
+    if('mcast' === self.options.get('proto')){
+      self.socket.addMembership(self.options.get('mcast.address'))
+      self.socket.setMulticastTTL(self.options.get('mcast.ttl'))
+    }
+    self.socket.on('message',function(buf,rinfo){
       var sum = buf.readInt32BE(0)
       buf = buf.slice(4)
       if(sum !== crc32.signed(buf)){
-        if(this.options.exists('logger'))
-          this.options.logger.warn('BAD CRC: ' + rinfo)
-        return false
+        self.emit('warning','BAD CRC: ' + rinfo)
+      } else {
+        var pkt = decode(buf)
+        pkt.rinfo = rinfo
+        //run middleware
+        async.eachSeries(self.middleware.receive,function(fn,next){fn(pkt,next)},function(err){
+          if(err) self.emit('error',err)
+          else self.emit('receive',pkt)
+        })
       }
-      var pkt = bencode.decode(buf)
-      for(var k in pkt)
-        if(pkt.hasOwnProperty(k) && Buffer.isBuffer(pkt[k]))
-          pkt[k] = pkt[k].toString()
-      pkt.rinfo = rinfo
-      this.callbacks.recv.some(function(cb){
-        return this.callbackReturnValues.recv.push(cb(pkt))
-      }.bind(this))
-    }.bind(this))
-  }.bind(this))
+    })
+  })
+}
+util.inherits(Communicator,EventEmitter)
+
+/**
+ * Configuration Defaults
+ * @type {{proto: string, mcast: {address: null, ttl: number}, address: string, port: number}}
+ */
+Communicator.prototype.optionSchema = {
+  proto: 'udp4',
+  mcast: {address: null, ttl: 1},
+  address: '127.0.0.1',
+  port: 3333
 }
 
-Communicator.prototype.sendPacket = function(payload){
-  var message = new ObjectManage(payload)
-  if(!message.exists('hostname')) message.set('hostname',this.options.hostname)
-  if(!message.exists('handle')) message.set('handle',this.options.handle)
-  if(!message.exists('sent')) message.set('sent',new Date().getTime())
-  var pkt = bencode.encode(message)
-  var buf = Buffer.concat([crc32(pkt),pkt])
-  this.socket.send(buf,0,buf.length,this.options.port,this.options.address)
+Communicator.prototype.middleware = {
+  send: [],
+  receive: []
+}
+
+/**
+ * Add send of receive middleware
+ * @param position  Position of the middleware either send or receive
+ * @param fn
+ */
+Communicator.prototype.use = function(position,fn){
+  var self = this
+  if('function' === typeof position){
+    fn = position
+    position = 'receive'
+  }
+  if('send' !== position || 'receive' !== position) position = 'receive'
+  self.middleware[position].push(fn)
+}
+
+/**
+ * Shortcut to add send middleware
+ * @param fn
+ */
+Communicator.prototype.useSend = function(fn){
+  this.use('send',fn)
+}
+
+/**
+ * Shortcut to add receive middleware
+ * @param fn
+ */
+Communicator.prototype.useReceive = function(fn){
+  this.use('receive',fn)
+}
+
+/**
+ * Send a payload via the socket
+ * @param payload  Message to be sent, either object or string
+ * @param done  Callback when message is sent
+ */
+Communicator.prototype.send = function(payload,done){
+  var self = this
+  if('string' === typeof payload) payload = {message: payload}
+  if('object' !== typeof payload) done('Invalid payload type, must be string or object')
+  else {
+    var message = new ObjectManage(payload)
+    if(!message.exists('hostname')) message.set('hostname',self.options.get('hostname'))
+    if(!message.exists('handle')) message.set('handle',self.options.get('handle'))
+    if(!message.exists('sent')) message.set('sent',new Date().getTime())
+    //run middleware
+    async.eachSeries(self.middleware.send,function(fn,next){fn(message,next)},function(err){
+      if(err) done(err)
+      else{
+        var buf = encode(message.get())
+        self.socket.send(buf,0,buf.length,self.options.port,self.options.address,done)
+      }
+    })
+  }
 }
 
 module.exports = Communicator
