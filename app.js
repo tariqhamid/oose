adac83a98ef4e717fb691227538c9e20b33dc9e4
Bryan Tong <contact@nullivex.com>
2014-03-24 16:00:37 -0600

    ideas for mesh middleware

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var mesh = require('./mesh')
   , serve = require('./serve')
   , resolve = require('./resolve')
 
-//start mesh for discovery and communication
-mesh.start()
 
 //start serve if its enabled
 if(config.get('serve.enabled')){
+  mesh.use(serve.annaounce())
   serve.start()
 }
 
 //start resolve if its enabled
 if(config.get('resolve.enabled')){
+  mesh.use(resolve.announce())
   resolve.start()
 }
+
+//start mesh for discovery and communication
+mesh.start()
