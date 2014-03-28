98d92adc06207385040372e35852c81816033766
Bryan Tong <contact@nullivex.com>
2014-03-28 14:45:17 -0600

    fixed export so it starts, missed renamed

--- export/index.js
+++ export/index.js
@@ -46,12 +46,12 @@ app.get('/:sha1/:filename',function(req,res){
 })
 
 exports.start = function(done){
-  app.listen(config.get('serve.port'),config.get('serve.host'),done)
+  app.listen(config.get('export.port'),config.get('export.host'),done)
 }
 
 if(require.main === module){
   exports.start(function(){
     var logger = require('../helpers/logger')
-    logger.info('Serve  started listening on port ' + config.get('serve.port'))
+    logger.info('Export started listening on port ' + config.get('export.port'))
   })
 }
