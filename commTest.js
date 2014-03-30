97e2b83f0d1e2bcb3aa222b2e2d9dc2b296af66b
Bryan Tong <contact@nullivex.com>
2014-03-30 15:56:32 -0600

    added a test example of using multicast to sha1 arp lookups and deciding a sliding window size using recent ping results

---
+++ commTest.js
@@ -0,0 +1,150 @@
+'use strict';
+var net = require('net')
+  , dgram = require('dgram')
+  , bencode = require('bencode')
+
+var multicastAddress = {
+  host: '224.0.0.110',
+  port: 9000
+}
+
+//tcp server
+var tcpServer = function(){
+  var tcp = net.createServer()
+  tcp.on('connection',function(socket){
+    console.log('Got a TCP connection')
+    //do some server stuff here
+  })
+  tcp.listen(9000,function(){
+    console.log('TCP listening on 9000')
+  })
+}
+
+//udp
+var udpServer = function(hostname){
+  var multicast = dgram.createSocket('udp4')
+  multicast.bind(multicastAddress.port,function(){
+    multicast.addMembership(multicastAddress.host,'127.0.0.1')
+    console.log('UDP listening on ' + multicastAddress.port + ' with multicast on ' + multicastAddress.host)
+  })
+  multicast.on('message',function(packet,rinfo){
+    var message = JSON.parse(bencode.decode(packet))
+    var response, buf
+    if('lookup' === message.command){
+      //lets say we have the sha1 so we need to response
+      response = {
+        command: 'lookup',
+        sha1: message.sha1,
+        who: {
+          hostname: hostname,
+          host: '127.0.0.1',
+          port: multicastAddress.port
+        }
+      }
+      buf = bencode.encode(JSON.stringify(response))
+      multicast.send(buf,0,buf.length,message.tell.port,message.tell.host,function(){
+        console.log('Sent UDP lookup response')
+      })
+    } else if('ping' === message.command){
+      response = {
+        command: 'ping'
+      }
+      buf = bencode.encode(JSON.stringify(response))
+      multicast.send(buf,0,buf.length,message.tell.port,message.tell.host,function(){
+        console.log('Send UDP ping response')
+      })
+    } else {
+      //ignore the packet since we dont know the command
+    }
+  })
+}
+
+var ping = {}
+var maxPing = function(){
+  var max = 0
+  for(var i in ping){
+    if(ping.hasOwnProperty(i) && ping[i] > max){
+      max = ping[i]
+    }
+  }
+  return max
+}
+var pingHosts = function(){
+  var start
+  var socket = dgram.createSocket('udp4')
+  socket.bind(function(){
+    socket.addMembership(multicastAddress.host,'127.0.0.1')
+    var message = {
+      command: 'ping',
+      tell: {
+        host: '127.0.0.1',
+        port: socket.address().port
+      }
+    }
+    var buf = bencode.encode(JSON.stringify(message))
+    socket.send(buf,0,buf.length,multicastAddress.port,multicastAddress.host,function(){
+      start = new Date().getTime()
+      console.log('Sent ping request')
+      setTimeout(pingHosts,1000)
+    })
+  })
+  socket.on('message',function(packet,rinfo){
+    var message = JSON.parse(bencode.decode(packet))
+    if('ping' === message.command){
+      ping[rinfo.address] = new Date().getTime() - start
+    }
+  })
+}
+
+//lets try a udp send and response kind of deal, we are going to encode with json
+var sha1Lookup = function(sha1,done){
+  //setup our receive
+  var who = []
+  var socket = dgram.createSocket('udp4')
+  socket.bind(function(){
+    socket.addMembership('224.0.0.110','127.0.0.1')
+    var message = {
+      command: 'lookup',
+      sha1: sha1,
+      tell: {
+        host: '127.0.0.1',
+        port: socket.address().port
+      }
+    }
+    var buf = bencode.encode(JSON.stringify(message))
+    socket.send(buf,0,buf.length,multicastAddress.port,multicastAddress.host,function(){
+      var windowSize = maxPing() * 2
+      console.log('Sent UDP Lookup Request with a window size of ' + windowSize)
+      console.log('Window size ')
+      setTimeout(function(){
+        done(who)
+      },windowSize)
+    })
+  })
+  //wait for the response here
+  socket.on('message',function(packet,rinfo){
+    var message = JSON.parse(bencode.decode(packet))
+    if('lookup' === message.command && sha1 === message.sha1){
+      who.push(message.who)
+    }
+  })
+}
+
+//wire up
+udpServer('blah1')
+udpServer('blah2')
+udpServer('blah3')
+
+//start pinging hosts too keep track of latency
+pingHosts()
+
+//send request
+setTimeout(function(){
+  var sha1 = 'foo'
+  sha1Lookup(sha1,function(who){
+    console.log(who)
+    process.exit()
+  })
+},2000)
+
+
