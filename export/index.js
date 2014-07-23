cb5096d6ef86996e950ac7f61b8128a679507434
Bryan Tong <contact@nullivex.com>
2014-07-23 14:04:24 -0600

    rewrite of export to use async

--- export/index.js
+++ export/index.js
@@ -6,69 +6,105 @@ var express = require('express')
   , fs = require('fs')
   , config = require('../config')
   , file = require('../helpers/file')
+  , Sniffer = require('../helpers/Sniffer')
   , FFmpeg = require('fluent-ffmpeg')
+  , async = require('async')
   , running = false
 
 app.get('/:sha1/:filename',function(req,res){
-  var sha1 = req.params.sha1
-  if(!sha1){
-    res.send('Invalid path')
-  } else {
-    var path = file.pathFromSha1(sha1)
-    //TODO: convert to async.series
-    redis.hgetall('inventory:' + sha1,function(err,info){
-      if(err){
-        console.log(err)
-        res.send(err)
-      } else if(!info){
-        res.status(404)
-        res.send(sha1 + ': File not found')
-      } else {
-        //convert stats to an object
-        var stat = JSON.parse(info.stat)
-        if(!fs.existsSync(path)){
-          res.status(404)
-          res.send('File not found')
-        } else {
-          //update hits
-          redis.hincrby('inventory:' + sha1,'hits',1)
-          //add attachment for a download
-          if('string' === typeof req.query.download){
-            res.set('Content-Disposition','attachment; filename=' + req.params.filename)
-          }
-          //set headers
-          res.set('Accept-Ranges','bytes')
-          res.set('Content-Type',info.mimeType)
-          //start param support
-          if(req.query.start && 'video/mp4' === info.mimeType){
-            var ffmpeg = new FFmpeg({source: path})
-            ffmpeg.setStartTime(req.query.start)
-            ffmpeg.writeToStream(res,{end: true})
-            return
-          }
-          //byte range support
-          var range = {start: 0, end: stat.size - 1}
-          var rangeRaw = req.get('range')
-          if(rangeRaw){
-            var match = rangeRaw.match(/(\d+)-(\d*)/)
-            if(match[1]) range.start = parseInt(match[1],10)
-            if(match[2]) range.end = parseInt(match[2],10)
-            res.status(206)
-            res.set('Content-Range','bytes ' + range.start + '-' + range.end + '/' + stat.size)
-          }
-          //set content length here
-          res.set('Content-Length',(range.end - range.start) + 1)
-          //setup read stream from the file
-          var rs = fs.createReadStream(path,range)
-          //update bytes sent
-          rs.on('data',function(data){
-            redis.hincrby('inventory:' + sha1,'sent',data.length)
-          })
-          rs.pipe(res)
+  var sha1, filename, path, info, stat, range
+  async.series(
+    [
+      //get sha1 and filename
+      function(next){
+        sha1 = req.params.sha1
+        filename = req.params.filename
+        if(!sha1) next('Invalid path')
+        if(!filename) filename = sha1
+        path = file.pathFromSha1(sha1)
+        next()
+      },
+      //check if path exists
+      function(next){
+        fs.exists(path,function(exists){
+          if(!exists) return next({code: 404, message: 'File doesnt exist'})
+          next()
+        })
+      },
+      //get inventory for file
+      function(next){
+        redis.hgetall('inventory:' + sha1,function(err,result){
+          if(err) return next(err)
+          if(!result) return next({code: 404,message: 'File not found'})
+          info = result
+          next()
+        })
+      },
+      //convert stats to an object
+      function(next){
+        stat = JSON.parse(info.stat)
+        next()
+      },
+      //update hits
+      function(next){
+        redis.hincrby('inventory:' + sha1,'hits',1,function(err){
+          if(err) return next(err)
+          next()
+        })
+      },
+      //set headers
+      function(next){
+        //add attachment for a download
+        if('string' === typeof req.query.download){
+          res.set('Content-Disposition','attachment; filename=' + req.params.filename)
         }
+        res.set('Accept-Ranges','bytes')
+        res.set('Content-Type',info.mimeType)
+        //byte range support
+        range = {start: 0, end: stat.size - 1}
+        var rangeRaw = req.get('range')
+        if(rangeRaw){
+          var match = rangeRaw.match(/(\d+)-(\d*)/)
+          if(match[1]) range.start = parseInt(match[1],10)
+          if(match[2]) range.end = parseInt(match[2],10)
+          res.status(206)
+          res.set('Content-Range','bytes ' + range.start + '-' + range.end + '/' + stat.size)
+        }
+        next()
       }
-    })
-  }
+
+    ],
+    function(err){
+      if(err){
+        if('string' === typeof err){
+          err = {code: 500, message: err}
+        }
+        console.log(err)
+        res.status(err.code)
+        res.send(err.message)
+      }
+      //setup output sniffer to track bytes sent regardless of output medium
+      var sniff = new Sniffer()
+      sniff.on('data',function(data){
+        redis.hincrby('inventory:' + sha1,'sent',data.length)
+      })
+      //setup sniffer to write to res
+      sniff.pipe(res)
+      //start param support
+      if(req.query.start && 'video/mp4' === info.mimeType){
+        var ffmpeg = new FFmpeg({source: path})
+        ffmpeg.setStartTime(req.query.start)
+        //tell ffmpeg to write to our sniffer
+        ffmpeg.writeToStream(sniff,{end: true})
+        return
+      }
+      //set content length here
+      res.set('Content-Length',(range.end - range.start) + 1)
+      //setup read stream from the file
+      var rs = fs.createReadStream(path,range)
+      rs.pipe(sniff)
+    }
+  )
 })
 
 
