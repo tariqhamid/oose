882a2afa562aa9a49c1bec3b53b1edfe34ae7f49
Bryan Tong <contact@nullivex.com>
2014-08-24 23:24:20 -0600

    closes #61

--- shredder/index.js
+++ shredder/index.js
@@ -1,13 +1,14 @@
 'use strict';
 var async = require('async')
+var childProcess = require('child_process')
 var fs = require('graceful-fs')
 var mkdirp = require('mkdirp')
 var moment = require('moment')
 var os = require('os')
 var path = require('path')
+var util = require('util')
 
 var commUtil = require('../helpers/communicator').util
-var Job = require('./helpers/job')
 var Logger = require('../helpers/logger')
 var shortId = require('../helpers/shortid')
 var logger = Logger.create('shredder')
@@ -21,133 +22,24 @@ var running = false
 
 
 /**
- * Check a job for a cached result
- * @param {Job} job
- * @param {function} next
+ * Spawn a worker to process the job
+ * @param {object} opts
  * @param {function} done
  */
-var jobCacheCheck = function(job,next,done){
-  job.cacheCheck(function(err,result){
-    if(err){
-      job.logger.info('Cache hit failed: ' + err)
-      return next()
-    }
-    if(!result) return next()
-    job.logger.info('Cache hit successful!',result)
-    job.update({
-      status: 'complete',
-      message: 'Complete, cache hit found',
-      steps: {
-        complete: 1,
-        total: 1
-      },
-      frames: {
-        complete: 1,
-        total: 1
-      },
-      resources: result
-    },true)
+var spawnWorker = function(opts,done){
+  logger.info('Starting to spawn worker for job ' + opts.handle)
+  var worker = childProcess.fork(__dirname + '/worker.js')
+  worker.on('error',function(err){
+    done(util.inspect(err))
+  })
+  worker.on('exit',function(code){
+    if(0 !== code)
+      return done('Worker failed, exited with code: ' + code)
+    logger.info('Worker for job ' + opts.handle + ' has finished and exited without error')
     done()
   })
-}
-
-
-/**
- * Run a job
- * @param {object} job Job structure
- * @param {function} done Callback
- */
-var runJob = function(job,done){
-  job.logger.info('Starting to process job')
-  async.series(
-    [
-      //check to see if this job was already processed in the past
-      function(next){
-        jobCacheCheck(job,next,done)
-      },
-      //step 2: obtain resources
-      function(next){
-        job.update({
-          status: 'resource',
-          message: 'Obtaining resources',
-          steps: {
-            complete: 0,
-            total: 3
-          }
-        },true)
-        job.obtainResources(next)
-      },
-      //now that we have resources check the cache again
-      function(next){
-        jobCacheCheck(job,next,done)
-      },
-      //step 3: execute encoding operations
-      function(next){
-        job.update({
-          status: 'encode',
-          message: 'Executing encoding jobs',
-          steps: {
-            complete: 1,
-            total: 3
-          },
-          frames: {
-            complete: 0,
-            total: 1
-          }
-        },true)
-        job.encode(next)
-      },
-      //step 4: save any resources after processing has finished
-      function(next){
-        job.update({
-          status: 'saving',
-          message: 'Saving resources',
-          steps: {
-            complete: 2,
-            total: 3
-          },
-          frames: {
-            complete: 0,
-            total: 1
-          }
-        },true)
-        job.save(function(err,result){
-          if(err) return next(err)
-          job.update({
-            resources: result
-          },true)
-          next()
-        })
-      },
-      //step 5: save the build to cache
-      function(next){
-        job.cacheStore(function(err){
-          if(err) job.logger.info('Cache store failed: ' + err)
-          next()
-        })
-      }
-    ],
-    function(err){
-      //cleanup the resources here since we dont it need any more
-      job.resource.cleanup()
-      if(err){
-        job.update({
-          status: 'error',
-          message: err
-        },true)
-        return done()
-      }
-      job.update({
-        status: 'complete',
-        message: 'Processing complete',
-        steps: {
-          complete: 3,
-          total: 3
-        }
-      },true)
-      done()
-    }
-  )
+  //start the worker by sending it options
+  worker.send({options: opts})
 }
 
 
@@ -158,13 +50,7 @@ var q = async.queue(
   function(opts,done){
     if('undefined' === typeof opts.handle || null === opts.handle || !opts.handle)
       done('ERROR: Job.handle not set')
-    //setup our job maintainer
-    var job = new Job(opts.handle,opts.description)
-    runJob(job,function(err){
-      if(err) job.logger.error('Job processing failed: ' + err)
-      else job.logger.info('Job processing successful')
-      done()
-    })
+    spawnWorker(opts,done)
   },
   config.shredder.concurrency || os.cpus().length || 1
 )
