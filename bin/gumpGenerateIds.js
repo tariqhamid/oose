20fb1087a93681c035bd0b9996a7b3fa6c016abd
Bryan Tong <contact@nullivex.com>
2014-08-28 21:36:06 -0400

    fix the gump id generator

--- bin/gumpGenerateIds.js
+++ bin/gumpGenerateIds.js
@@ -1,4 +1,5 @@
 'use strict';
+var async = require('async')
 var mongoose = require('mongoose')
 
 var logger = require('../helpers/logger').create('gump:generateIds')
@@ -10,22 +11,28 @@ var config = require('../config')
 mongoose.connect(config.mongoose.dsn,config.mongoose.options,function(err){
   if(err) throw err
   logger.info('Starting to fill in missing file IDs for Gump')
-  var stream = File.find().stream()
-  stream.on('error',function(err){
-    logger.error(err)
-    process.exit()
-  })
-  stream.on('data',function(doc){
-    if(!doc.handle) doc.handle = shortid.generate()
-    doc.save(function(err){
-      if(err){
-        logger.error(err.message)
+  var stream = File.find({},function(err,results){
+    if(err){
+      logger.error(err)
+      process.exit()
+    }
+    async.each(
+      results,
+      function(doc,next){
+        if(!doc.handle) doc.handle = shortid.generate()
+        console.log(doc.id,doc.handle)
+        doc.save(function(err){
+          if(err) return next(err.message)
+          next()
+        })
+      },
+      function(err){
+        if(err){
+          logger.error(err)
+        }
+        logger.info('Finished generating missing IDs')
         process.exit()
       }
-    })
-  })
-  stream.on('end',function(){
-    logger.info('Finished generating missing IDs')
-    process.exit()
+    )
   })
 })
