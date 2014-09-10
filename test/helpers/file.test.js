71dcef783a5450325b61a6f40afb24d940be34b4
Bryan Tong <contact@nullivex.com>
2014-09-09 23:28:57 -0600

    dropped communicator test, stubbed out file test

---
+++ test/helpers/file.test.js
@@ -0,0 +1,15 @@
+'use strict';
+//var expect = require('chai').expect
+
+//var file = require('../../helpers/file')
+
+
+describe('helpers/file',function(){
+  it('should create a sha1 sum of a path')
+  it('should create a path from a sha1')
+  it('should create a sha1 from a path')
+  it('should insert the file into redis')
+  it('should write a new file from a source path')
+  it('should write a new file from a readable stream')
+  it('should write a new file from source path without sha1')
+})
