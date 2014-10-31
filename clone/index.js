abf92ec377fbec82e3da9501612451a1ef6727f6
Bryan Tong <contact@nullivex.com>
2014-10-31 15:06:28 -0600

    closes #149

--- clone/index.js
+++ clone/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var async = require('async')
+var P = require('bluebird')
 var axon = require('axon')
 var crypto = require('crypto')
 var debug = require('debug')('oose:clone')
@@ -10,7 +11,6 @@ var prettyBytes = require('pretty-bytes')
 var promisePipe = require('promisepipe')
 var through2 = require('through2')
 
-
 var file = require('../helpers/file')
 var logger = require('../helpers/logger').create('clone')
 var peer = require('../helpers/peer')
@@ -18,94 +18,68 @@ var peer = require('../helpers/peer')
 var config = require('../config')
 var server = axon.socket('rep')
 
+//make some promises
+P.promisifyAll(fs)
+P.promisifyAll(server)
+
 
 /**
  * Clone a file to another peer
  * @param {object} job
- * @param {function} cb
+ * @return {P}
  */
-var clone = function(job,cb){
-  if('function' !== typeof cb) cb = function(){}
-  var done = function(err){
-    if(err) logger.error(job.sha1 + ' failed to replicate: ' + err)
-    cb(err)
-  }
+var clone = function(job){
   //track some stats
   var winner
   var bytesSent = 0
   var filePath = file.pathFromSha1(job.sha1)
   var start = +(new Date())
   var shasum = crypto.createHash('sha1')
-  //start process
-  async.series(
-    [
-      //get peer
-      function(next){
-        peer.next(config.hostname,function(err,result){
-          if(err) return next(err)
-          if(!result) return next('No suitable peer found')
-          winner = result
-          next()
-        })
-      },
-      //check file existence
-      function(next){
-        fs.exists(filePath,function(exists){
-          if(!exists) return next('File not found for clone')
-          next()
-        })
-      },
-      //wire up pipe
-      function(next){
-        logger.info('Starting to replicate ' + job.sha1)
-        //setup a sniffer to capture the sha1 for integrity
-        var sniff = through2(
-          function(chunk,enc,next){
-            try {
-              bytesSent = bytesSent + chunk.length
-              shasum.update(chunk)
-              next(null,chunk)
-            } catch(err){
-              next(err)
-            }
-          }
-        )
-        //setup readable from file system
-        var rs = fs.createReadStream(filePath)
-        //connect to our per for writing
-        var ws = net.connect(+winner.portImport,winner.ip)
-        promisePipe(rs,sniff,ws).then(
-          function(){
-            var sha1 = shasum.digest('hex')
-            var duration = ((+new Date()) - start) / 1000
-            if(!bytesSent) bytesSent = 0
-            if(!duration) duration = 1
-            var bytesPerSec = prettyBytes(bytesSent / duration) + '/sec'
-            logger.info(
-              'Finished replicating ' +
-              prettyBytes(bytesSent) + ' ' +
-              job.sha1 +
-              ' as ' + sha1 +
-              ' to ' + winner.hostname + '.' + winner.domain +
-              ' in ' + duration + ' seconds ' +
-              'averaging ' + bytesPerSec)
-            next(null,sha1)
-          },
-          function(err){
-            next('Failed in stream ' + err.source + ': ' + err.message)
-          }
-        )
-      }
-    ],
-    function(err){
-      done(err)
+  var sniff = through2(function(chunk,enc,next){
+    try {
+      bytesSent = bytesSent + chunk.length
+      shasum.update(chunk)
+      next(null,chunk)
+    } catch(err){
+      next(err)
     }
-  )
+  })
+  return peer.next(config.hostname)
+    .then(function(result){
+      if(!result) throw new Error('No suitable peer found')
+      winner = result
+      if(!fs.existsSync(filePath)) throw new Error('File to clone doesnt exist')
+      logger.info('Starting to replicate ' + job.sha1)
+      //setup readable from file system
+      var rs = fs.createReadStream(filePath)
+      //connect to our per for writing
+      var ws = net.connect(+winner.portImport,winner.ip)
+      return promisePipe(rs,sniff,ws)
+    })
+    .then(function(){
+      var sha1 = shasum.digest('hex')
+      var duration = ((+new Date()) - start) / 1000
+      if(!bytesSent) bytesSent = 0
+      if(!duration) duration = 1
+      var bytesPerSec = prettyBytes(bytesSent / duration) + '/sec'
+      logger.info(
+        'Finished replicating ' +
+        prettyBytes(bytesSent) + ' ' +
+        job.sha1 +
+        ' as ' + sha1 +
+        ' to ' + winner.hostname + '.' + winner.domain +
+        ' in ' + duration + ' seconds ' +
+        'averaging ' + bytesPerSec)
+      return sha1
+    })
 }
 
-var q = async.queue(clone,config.clone.concurrency || 1)
-//TESTING
-//q.push({sha1: '77e89411d8747bcc6003bdd35768adeddfbca4cd'})
+var q = async.queue(
+  function(job,done){
+    clone(done).then(function(){done()},done)
+  },
+  config.clone.concurrency || 1
+)
 
 
 /**
