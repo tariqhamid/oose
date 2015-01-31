d40b5ce1a176ec9ed4a5f74c5d7962cdac2ca41d
Bryan Tong <contact@nullivex.com>
2015-01-30 22:02:23 -0700

    allow basic auth to override session driven prism functions

--- helpers/userSessionValidate.js
+++ helpers/userSessionValidate.js
@@ -1,4 +1,5 @@
 'use strict';
+var basicAuth = require('connect-basic-auth')
 var debug = require('debug')('oose:userSessionValidate')
 var oose = require('oose-sdk')
 
@@ -9,61 +10,69 @@ var UserError = oose.UserError
 
 var config = require('../config')
 
+var auth = basicAuth(config.prism.username,config.prism.password)
+
 
 /**
  * Validate User Session Middleware
  * @param {object} req
  * @param {object} res
  * @param {function} next
+ * @return {*}
  */
 module.exports = function(req,res,next){
   var token = req.get(config.master.user.sessionTokenName) || ''
   debug('got token',token)
-  var session
-  redis.getAsync(redis.schema.userSession(token))
-    .then(function(result){
-      if('string' !== typeof token || 64 !== token.length)
-        throw new UserError('Invalid session token passed')
-      if(!result){
-        debug('cache miss',token)
-        var client = api.master()
-        return client.postAsync({
-          url: client.url('/user/session/validate'),
-          json: {
-            token: token,
-            ip: req.ip
-          }
-        })
-          .spread(function(res,body){
-            if(!body)
-              throw new UserError('Session doesnt exist')
-            if('Session valid' !== body.success)
-              throw new UserError('Invalid session')
-            session = body.session
-            return redis.setAsync(
-              redis.schema.userSession(token),JSON.stringify(session))
+  //without a token lets try basic auth since it can override
+  if(!token){
+    auth(req,res,next)
+  } else {
+    var session
+    redis.getAsync(redis.schema.userSession(token))
+      .then(function(result){
+        if('string' !== typeof token || 64 !== token.length)
+          throw new UserError('Invalid session token passed')
+        if(!result){
+          debug('cache miss',token)
+          var client = api.master()
+          return client.postAsync({
+            url: client.url('/user/session/validate'),
+            json: {
+              token: token,
+              ip: req.ip
+            }
           })
-          .catch(client.handleNetworkError)
-      } else {
-        debug('cache hit',token)
-        session = JSON.parse(result)
-      }
-    })
-    .then(function(){
-      req.session = session
-      next()
-    })
-    .catch(SyntaxError,function(err){
-      res.status(500)
-      res.json({error: 'Failed to parse session record from redis: ' +
+            .spread(function(res,body){
+              if(!body)
+                throw new UserError('Session doesnt exist')
+              if('Session valid' !== body.success)
+                throw new UserError('Invalid session')
+              session = body.session
+              return redis.setAsync(
+                redis.schema.userSession(token),JSON.stringify(session))
+            })
+            .catch(client.handleNetworkError)
+        } else {
+          debug('cache hit',token)
+          session = JSON.parse(result)
+        }
+      })
+      .then(function(){
+        req.session = session
+        next()
+      })
+      .catch(SyntaxError,function(err){
+        res.status(500)
+        res.json({error: 'Failed to parse session record from redis: ' +
         err.message})
-    })
-    .catch(NetworkError,function(err){
-      res.status(500)
-      res.json({error: 'Failed to validate session: ' + err.message})
-    })
-    .catch(UserError,function(err){
-      res.status(401)
-      res.json({error: err.message})
-    })
+      })
+      .catch(NetworkError,function(err){
+        res.status(500)
+        res.json({error: 'Failed to validate session: ' + err.message})
+      })
+      .catch(UserError,function(err){
+        res.status(401)
+        res.json({error: err.message})
+      })
+  }
 }
