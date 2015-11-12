d035700d77a4f452c41879ad11f39a8efec9a421
Bryan Tong <contact@nullivex.com>
2015-11-11 21:06:15 -0700

    store register and mark unavail on shutdown

--- store/index.js
+++ store/index.js
@@ -2,32 +2,65 @@
 var P = require('bluebird')
 var infant = require('infant')
 
+var config = require('../config')
+var cradle = require('../helpers/couchdb')
+
 var cluster
 var inventory
 var purchase
-var config = require('../config')
+var storeKey = cradle.schema.store(config.store.name)
+
+//make some promises
+P.promisifyAll(infant)
 
 if(require.main === module){
   infant.child(
     'oose:' + config.store.name + ':master',
     function(done){
-      cluster = P.promisifyAll(infant.clusterSetup(
+      console.log('Beginning store startup')
+      //bootstrap to start
+      cluster = infant.cluster(
         './worker',
         {
           enhanced: true,
           count: config.store.workers.count,
           maxConnections: config.store.workers.maxConnections
         }
-      ))
-      inventory = P.promisifyAll(infant.child('./inventory'))
-      purchase = P.promisifyAll(infant.child('./purchase'))
+      )
+      inventory = infant.parent('./inventory')
+      purchase = infant.parent('./purchase')
+      //fire everything up
       P.all([
         cluster.startAsync(),
         inventory.startAsync(),
         purchase.startAsync()
       ])
         .then(function(){
-          done()
+          //now register ourselves or mark ourselves available
+          return cradle.db.getAsync(storeKey)
+        })
+        .then(
+          //if we exist lets mark ourselves available
+          function(doc){
+            doc.available = true
+            return cradle.db.saveAsync(storeKey,doc._rev,doc)
+          },
+          //if we dont exist lets make sure thats why and create ourselves
+          function(err){
+            if(404 !== err.headers.status) throw err
+            return cradle.db.saveAsync(storeKey,{
+              name: config.store.name,
+              host: config.store.host,
+              port: config.store.port,
+              writable: true,
+              available: true,
+              active: true,
+              createdAt: new Date().toJSON()
+            })
+          }
+        )
+        .then(function(){
+          console.log('Store startup complete')
         })
         .catch(function(err){
           console.log(err.stack)
@@ -36,10 +69,24 @@ if(require.main === module){
         })
     },
     function(done){
-      if(!cluster) return done()
-      cluster.stop(function(err){
-        done(err)
-      })
+      //mark ourselves as down
+      cradle.db.getAsync(storeKey)
+        .then(function(doc){
+          doc.available = false
+          return cradle.db.saveAsync(storeKey,doc._rev,doc)
+        })
+        .then(function(){
+          if(!cluster) return
+          return cluster.stopAsync()
+        })
+        .then(function(){
+          done()
+        })
+        .catch(function(err){
+          console.log(err.stack)
+          console.log(err)
+          done(err)
+        })
     }
   )
 }
