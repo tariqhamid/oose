0374f8ea81ee26d02e4755aca35e8c7cb227a6ad
Bryan Tong <contact@nullivex.com>
2016-02-23 17:44:24 -0700

    kill some debug

--- bin/couchMigrator.js
+++ bin/couchMigrator.js
@@ -55,9 +55,7 @@ var migrateItems = function(name,itemKey,dbName,keyFunc,filterFunc){
       endkey: itemKey + '\uffff'
     }
     debug('creating read stream',readStreamOpts)
-    var readStream = cradle.oose.all(readStreamOpts,function(err,stuff){
-      console.log(err,stuff)
-    })
+    var readStream = cradle.oose.all(readStreamOpts)
     writeStream.on('data',function(chunk){
       result.push(chunk.id)
     })
