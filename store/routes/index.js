97906e92c7d4da3dbc603a407e09c73a18cafb00
Bryan Tong <contact@nullivex.com>
2016-11-10 16:36:31 -0700

    link up purchase routes

--- store/routes/index.js
+++ store/routes/index.js
@@ -55,3 +55,10 @@ exports.stats = function(req,res){
  * @type {object}
  */
 exports.content = require('./content')
+
+
+/**
+ * Purchase routes
+ * @type {object}
+ */
+exports.purchase = require('./purchase')
