850e017aac88df5390df354354842abd730b8235
Bryan Tong <contact@nullivex.com>
2016-11-15 17:35:24 -0700

    remove route protection from store purchase lookup

--- store/routes/purchase.js
+++ store/routes/purchase.js
@@ -11,19 +11,19 @@ var config = require('../../config')
  * @param {object} res
  */
 exports.uri = function(req,res){
-  if(config.store.host !== req.ip && '127.0.0.1' !== req.ip){
-    res.status(403).send('/403')
-  } else {
-    var token = req.params.token
-    purchasedb.get(token)
-      .then(function(result){
-        if(result && result.expirationDate >= (+new Date())){
-          res.send(
-            '/../content/' + hashFile.toRelativePath(result.hash,result.ext)
-          )
-        } else{
-          res.status(404).send('/404')
-        }
-      })
-  }
+  //if(config.store.host !== req.ip && '127.0.0.1' !== req.ip){
+  //  res.status(403).send('/403')
+  //} else {
+  var token = req.params.token
+  purchasedb.get(token)
+    .then(function(result){
+      if(result && result.expirationDate >= (+new Date())){
+        res.send(
+          '/../content/' + hashFile.toRelativePath(result.hash,result.ext)
+        )
+      } else{
+        res.status(404).send('/404')
+      }
+    })
+  //}
 }
