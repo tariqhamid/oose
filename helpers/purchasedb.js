66cdeda745023446e636d287a1a1921414b0adee
Bryan Tong <contact@nullivex.com>
2016-12-01 16:30:31 -0700

    add debugging to purchasedb

--- helpers/purchasedb.js
+++ helpers/purchasedb.js
@@ -1,6 +1,7 @@
 'use strict';
 var P = require('bluebird')
 var cradle = require('cradle')
+var debug = require('debug')('oose:purchasedb')
 var moment = require('moment')
 var oose = require('oose-sdk')
 var Password = require('node-password').Password
@@ -54,7 +55,9 @@ var getDatabaseName = function(token){
  * Build configuration
  */
 var buildConfig = function(){
+  debug('building config')
   Object.keys(config.prism.purchaseZoneCouch).forEach(function(zone){
+    debug('building config for zone',zone)
     var couchConfig = [
       {
         host: config.couchdb.host,
@@ -68,8 +71,10 @@ var buildConfig = function(){
     ){
       couchConfig = config.prism.purchaseZoneCouch[zone]
     }
+    debug('config for zone',zone,couchConfig)
     couchConfigs[zone] = couchConfig
   })
+  debug('config build complete',couchConfigs)
 }
 buildConfig()
 
@@ -80,9 +85,20 @@ buildConfig()
  * @return {Object|false}
  */
 var pickCouchConfig = function(zone){
-  if(!couchConfigs || !couchConfigs[zone]) return false
-  if(1 === couchConfigs[zone].length) return couchConfigs[zone][0]
-  return couchConfigs[zone][random.integer(0,(couchConfigs[zone].length - 1))]
+  debug('picking couch config',zone)
+  if(!couchConfigs || !couchConfigs[zone]){
+    debug('no configs',zone)
+    return false
+  }
+  if(1 === couchConfigs[zone].length){
+    debug('only one couch returning',zone)
+    return couchConfigs[zone][0]
+  }
+  debug('picking couch from zonelist',zone,couchConfigs[zone])
+  var winner = couchConfigs[zone][
+    random.integer(0,(couchConfigs[zone].length - 1))]
+  debug('winner picked',winner)
+  return winner
 }
 
 
@@ -96,11 +112,13 @@ var pickCouchConfig = function(zone){
  */
 var setupReplication = function(promises,databaseName,couchConfig,replConfig){
   //verify we are not the same server as currently being used
+  debug('setupReplication',databaseName,couchConfig,replConfig)
   if(
     replConfig.host === couchConfig.host &&
     replConfig.port === couchConfig.port
   )
   {
+    debug('replConfig matches couchConfig returning')
     return
   }
   var couchdb = new (cradle.Connection)(
@@ -115,35 +133,44 @@ var setupReplication = function(promises,databaseName,couchConfig,replConfig){
     replConfig.options
   )
   P.promisifyAll(repldb)
+  debug('scaffolded replication servers')
   promises.push(function(){
     return P.all([
       //from current to repl
       function(){
-        couchdb.database('_replicator')
-        return couchdb.saveAsync(
-          'oose-purchase-' + databaseName + '-' +
-          couchConfig.host + '->' +
-          replConfig.host,
-          {
-            source: 'oose-purchase-' + databaseName,
-            target: 'http://' + replConfig.host +
-            ':' + replConfig.port + '/' +
-            'oose-purchase-' + databaseName,
-            continuous: true,
-            use_checkpoints: true,
-            checkpoint_interval: '30',
-            owner: 'root'
-          }
-        )
+        debug('couchdb creating oose-purchase-' + databaseName)
+        repldb.database('oose-purchase-' + databaseName)
+        return repldb.createAsync()
+          .then(function(){
+            debug('couchdb database set to _replicator')
+            couchdb.database('_replicator')
+            debug('saving replicator from couch to repl',couchConfig,replConfig)
+            return couchdb.saveAsync(
+              'oose-purchase-' + databaseName + '-' +
+              couchConfig.host + '->' +
+              replConfig.host,
+              {
+                source: 'oose-purchase-' + databaseName,
+                target: 'http://' + replConfig.host +
+                ':' + replConfig.port + '/' +
+                'oose-purchase-' + databaseName,
+                continuous: true,
+                use_checkpoints: true,
+                checkpoint_interval: '30',
+                owner: 'root'
+              }
+            )
+          })
       },
       //from repl to current
       function(){
-        repldb.existsAsync('oose-purchase-' + databaseName)
-          .then(function(result){
-            if(!result) return repldb.createAsync()
-          })
+        debug('repldb creating oose-purchase-' + databaseName)
+        repldb.database('oose-purchase-' + databaseName)
+        return repldb.createAsync()
           .then(function(){
+            debug('repldb switching to db _replicator')
             repldb.database('_replicator')
+            debug('saving replicator from repl to couch',replConfig,couchConfig)
             repldb.saveAsync(
               'oose-purchase-' + databaseName + '-' +
               replConfig.host + '->' +
@@ -177,25 +204,13 @@ var createDatabase = function(token){
   var databaseName = getDatabaseName(token)
   var zone = getZone(token)
   var promises = []
+  debug('create database',token,zone,databaseName)
   couchConfigs[zone].forEach(function(couchConfig){
-    promises.push(function(){
-      var couchdb = new (cradle.Connection)(
-        couchConfig.host,
-        couchConfig.port,
-        couchConfig.options
-      )
-      P.promisifyAll(couchdb)
-      couchdb.database('oose-purchase-' + databaseName)
-      return couchdb.createAsync()
-        .then(function(){
-          //create replication to everyone
-          var promises = []
-          couchConfigs[zone].forEach(function(replConfig){
-            setupReplication(promises,couchdb,couchConfig,replConfig)
-          })
-        })
+    couchConfigs[zone].forEach(function(replConfig){
+      setupReplication(promises,databaseName,couchConfig,replConfig)
     })
   })
+  debug('promises set for creation',databaseName,promises)
   return P.all(promises)
 }
 
@@ -244,18 +259,27 @@ PurchaseDb.prototype.get = function(token){
   //get token
   var couchdb
   return P.try(function(){
+    debug(token,'get')
     couchdb = couchWrap(token)
+    debug(token,'couch wrapped',couchdb)
     if(!couchdb) throw new UserError('Could not validate purchase token')
     return couchdb.getAsync(token)
   })
+    .then(function(result){
+      debug(token,'get result',result)
+      return result
+    })
     .catch(function(err){
+      debug(token,'get error',err)
       if(!err.headers || !err.headers.status) throw err
       else if(404 === err.headers.status &&
         ('Database does not exist.' === err.reason ||
         'no_db_file' === err.reason)
       ){
+        debug(token,'creating database for get')
         return createDatabase(token)
           .then(function(){
+            debug(token,'database created reattempted get')
             return couchdb.getAsync(token)
           })
       } else throw err
@@ -269,11 +293,14 @@ PurchaseDb.prototype.get = function(token){
  * @return {promise}
  */
 PurchaseDb.prototype.exists = function(token){
+  debug(token,'exists')
   return this.get(token)
     .then(function(result){
+      debug(token,'exists result',result)
       return !!result
     })
-    .catch(function(){
+    .catch(function(err){
+      debug(token,'exists error',err)
       return false
     })
 }
@@ -288,19 +315,28 @@ PurchaseDb.prototype.exists = function(token){
 PurchaseDb.prototype.create = function(token,params){
   //create purchase
   var couchdb
+  debug(token,'create')
   return P.try(function(){
     couchdb = couchWrap(token)
     if(!couchdb) throw new UserError('Could not validate purchase token')
+    debug(token,'couch wrapped')
     return couchdb.saveAsync(token,params)
   })
+    .then(function(result){
+      debug(token,'create result',result)
+      return result
+    })
     .catch(function(err){
+      debug(token,'create error',err)
       if(!err.headers || !err.headers.status) throw err
       else if(404 === err.headers.status &&
         ('Database does not exist.' === err.reason ||
         'no_db_file' === err.reason)
       ){
+        debug(token,'creating database for create',params)
         return createDatabase(token)
           .then(function(){
+            debug(token,'database created reattempt create',params)
             return couchdb.saveAsync(token,params)
           })
       } else throw err
@@ -318,26 +354,34 @@ PurchaseDb.prototype.update = function(token,params){
   //update purchase
   var that = this
   var couchdb
+  debug(token,'update')
   return P.try(function(){
     couchdb = couchWrap(token)
     if(!couchdb) throw new UserError('Could not validate purchase token')
+    debug(token,'couch wrapped getting')
     return that.get(token)
   })
     .then(function(result){
-      if(result)
+      if(result){
+        debug(token,'update result received, udpating',result,params)
         return couchdb.saveAsync(token,result._rev,params)
-      else
+      } else{
+        debug(token,'doesnt exist, creating',result,params)
         that.create(token,params)
+      }
     })
     .catch(function(err){
+      debug(token,'update error',err)
       if(!err.headers || !err.headers.status) throw err
       else if(404 === err.headers.status &&
         ('Database does not exist.' === err.reason ||
         'no_db_file' === err.reason)
       ){
+        debug(token,'creating database for update',params)
         return createDatabase(token)
           .then(function(){
-            return couchdb.saveAsync(token,params)
+            debug(token,'database created reattempted update',params)
+            return that.create(token,params)
           })
       } else throw err
     })
@@ -351,11 +395,17 @@ PurchaseDb.prototype.update = function(token,params){
  */
 PurchaseDb.prototype.remove = function(token){
   //remove purchase
+  debug(token,'remove')
   return this.get(token)
     .then(function(result){
-      if(result)
+      debug(token,'remove result',result)
+      if(result){
+        debug(token,'remove exists, removing')
         return couchWrap(token).removeAsync(token,result._rev)
-      //otherwise it doesn't exist... cool
+      } else {
+        debug(token,'remove doesnt exist do nothing')
+        //otherwise it doesn't exist... cool
+      }
     })
 }
 
@@ -390,7 +440,9 @@ PurchaseDb.prototype.generate = function(zone){
     zone = config.prism.purchaseZone || 'a'
   var date = moment().format('YYYYMMDD')
   var salt = new Password({length: 11, special: false}).toString()
-  return zone.slice(0,1) + date.slice(0,8) + salt.slice(0,11)
+  var token = zone.slice(0,1) + date.slice(0,8) + salt.slice(0,11)
+  debug('generated token',token)
+  return token
 }
 
 
